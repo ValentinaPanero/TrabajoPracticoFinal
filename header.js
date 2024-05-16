8e793174d66ada8a22aca7341fde0ba6d164e76f
Panero Valentina <167832813+ValentinaPanero@users.noreply.github.com>
2024-05-16 04:45:31 -0300

    16/05 04:45am

---
+++ header.js
@@ -0,0 +1,14 @@
+document.write(` 
+<header>
+    <img class="logo" src="Img/Logo.jpg" alt="Logo" onclick="window.open('index.html', '_self')" />
+    <button id="abrir" class="abrir-menu"><i class="bi bi-list"></i></button>
+
+
+    <nav class="nav" id="nav">
+        <button class="cerrar-menu" id="cerrar"><i class="bi bi-x-circle"></i></button>
+        <ul class="nav-list">
+            <li><a href="productos.html">Productos</a></li>
+            <li><a href="tienda.html">Tienda</a></li>
+        </ul>
+    </nav>
+</header>`)
